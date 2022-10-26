e48595674467d55e44b0e7877f56faa859fa368a
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2022-10-26 10:44:59 +0300

    udemy exercise

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by valters.steinblums on 26/10/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
